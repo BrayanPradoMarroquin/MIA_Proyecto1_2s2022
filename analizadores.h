2d53fefa23a9a6218ef38841da9f7eea49328b3b
BrayanPradoMarroquin <2995454660101@ingenieria.usac.edu.gt>
2022-08-08 01:34:46 -0600

    mini analizador inicial

--- analizadores.h
+++ analizadores.h
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+void Analizador(string &i);
+void exec(string &i, string &root);
+
 void Analizador(string &i){
-    
+    printf("Hola como estas, soy el analizador ... \n");
+}
+
+void exec(string &i, string &root){
+    if (i=="exec")
+    {
+        printf("analizador del exec ... \n");
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
+#include "analizadores.h"
 using namespace std;
 
 int main(int argc, char *argv[]){
-    char* mensaje;
-
+    string mensaje;
+    string new_command;
+    mensaje = argv[1];
+   
     if(argc > 1){
-        mensaje = argv[1];
+        if (argc == 3)
+        {
+            string root = argv[2];
+            exec(mensaje, root);
+        }if (argc > 3)
+        {
+            cout<<"mayor a 3"<<endl;
+        }else
+        {
+            cout<<"faltan parametros en el comando"<<endl;
+        }
     }else{
         mensaje = "ejecute un comando para empezar ... \n";
     }
 
-    printf(mensaje);
+    Analizador(mensaje);
     return 0;
 }
